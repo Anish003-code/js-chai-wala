6cfb087b1a02b78f756a0a5b2167a17c54044637
Anish003-code <anishkumarm883@gmail.com>
2024-08-23 11:28:34 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,20 @@
+    let name = "anish"
+    let age = 21
+    let isloggedin = true;
+     
+    //DATA TYPE 
+    /* 
+    numeber ==> iska range 2 to power 53
+    bigint ==> this datatype is used for big numbers
+    string ==> "" or '' both are correct either use single qwuote or diublke quote
+    boolean ==. true/ false
+    null is a  standalone value  it is an object
+    
+    undefined ==>  meaning abhi value assing nhi hui hai
+    symbol =.> used to fing uniquness
+
+     object ==> 
+
+    */
+   console.log(typeof null);
+
