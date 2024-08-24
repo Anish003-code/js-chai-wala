f9e51e10d046487e9fefb0be1758461be515f2a9
Anish003-code <anishkumarm883@gmail.com>
2024-08-24 13:46:41 +0000

    datatype

---
+++ codewithharry/03primitvedatatype.js
@@ -0,0 +1,10 @@
+// there are 77 types of primitive datatype in the js 
+// they are 
+/* 
+n => null     b => boolean     s => string    u => undefined
+n => number     b => BigInt("")     s => symbol
+*/
+let c = BigInt("222");
+console.log(typeof c);
+
+// non - primivite datatype
