3922182f907f3c2f55fdfc7867b5a95a3a3a0e5c
Anish003-code <anishkumarm883@gmail.com>
2024-08-25 20:23:06 +0000

    conditional

---
+++ codewithharry/conditional.js
@@ -0,0 +1,29 @@
+// prompt puchta hai aur alert btata hai
+let a = prompt("hey what is your name");
+if(a == "anish"){
+    alert("wow");
+}
+// if else 
+// terniary operators'
+console.log("you can ", age < 18? "not drive" : "drove")
+// problems 
+// let age = prompt("what is your age");
+// if(age >=10 && age <= 20){
+//     alert("your age lie between the prefered range ");
+// }
+// problem number 2
+let age = prompt("enter your age")
+
+switch(age){
+    case 12: alert("your age is 12");
+    break;
+    case 13: alert("your age is 13");
+    break;
+
+    case 14: alert("your age is 14");
+    break;
+    case 15: alert("your age is 15");
+    break;
+    default: alert("chl n laude");
+    break;
+}
