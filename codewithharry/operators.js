00b97d1a7c26ba0df36996a12ee402b76da59615
Anish003-code <anishkumarm883@gmail.com>
2024-08-25 18:51:57 +0000

    operators

---
+++ codewithharry/operators.js
@@ -0,0 +1,51 @@
+// operators are basically the simple calculations that are incoled in the js 
+  let a = 99 , b = 2;
+console.log(a+b);
+    console.log(a-b);
+console.log(a/b);// unlike in c js gives the proper decimal values
+console.log(a*b);
+// exponential operator ** example a**b meaning a to the power b
+console.log(a**b);
+console.log(a%b);
+console.log(a++); // here after giving the value of a its value gets increased by 1 as it is called as increment  operator
+// same goes for the decrement operators 
+console.log(a);
+console.log(++a);
+// console.log(a);
+console.log(a);
+// arhenmatric operators 
+console.log(a+=b); // a = a+b;
+//comparison operators 
+let ac = 33
+let  bc = 33;
+console.log(ac == bc);
+console.log(ac != bc);
+console.log(ac === bc);
+ // the differnce between the == and === is that === is correct only if the value and type bot are equal
+console.log(ac !== bc);
+let A = 1;
+let B = 2
+console.log(A > B); // boolean expression denge mainly.
+console.log(A < B);
+console.log(A == B);
+// logical operators
+let c = 22
+let v = 3
+console.log(c > v && c > 11);
+console.log(c < v || c > v);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
